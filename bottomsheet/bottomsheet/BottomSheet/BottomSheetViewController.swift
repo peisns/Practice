8a565e4dab9d8a674f1c20992b8f41b4ca881535
Soul <hansol1501@gmail.com>
2025-03-10 14:25:21 +0900

    init

---
+++ bottomsheet/bottomsheet/BottomSheet/BottomSheetViewController.swift
@@ -0,0 +1,28 @@
+//
+//  BottomSheetViewController.swift
+//  bottomsheet
+//
+//  Created by buildbook on 3/5/25.
+//
+
+import UIKit
+
+final class BottomSheetViewController: UIViewController {
+    
+    @IBOutlet weak var bottomSheetOpenButton: UIButton!
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    @IBAction func bottomSheetOpenButtonClicked(_ sender: UIButton) {
+        
+    }
+}
+
+extension BottomSheetViewController {
+    
+    
+}
