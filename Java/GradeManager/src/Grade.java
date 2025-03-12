c33514d90b08b63d49055d28f4dd88897d56eeb8
soul <hansol1501@gmail.com>
2025-03-12 23:13:37 +0900

    feat: 성적처리 프로그램1

---
+++ Java/GradeManager/src/Grade.java
@@ -0,0 +1,14 @@
+
+public class Grade {
+	int e;
+	int m;
+	
+	void input_grade(int a, int b) {
+		e = a;
+		m = b;
+	}
+	
+	void output_grad() {
+		System.out.println(e+m);;
+	}
+}
---
+++ Java/GradeManager/src/GradeOutput.java
@@ -0,0 +1,14 @@
+
+public class GradeOutput {
+
+	public static void main(String[] args) {
+		Grade g1, g2;
+		g1 = new Grade();
+		g2 = new Grade();
+		g1.input_grade(90, 85);
+		g2.input_grade(80, 80);
+		
+		g1.output_grad();
+		g2.output_grad();
+	}
+}
