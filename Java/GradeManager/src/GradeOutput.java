029eda3bb3408c1e2ea10f2a379d69da5e1953a2
soul <hansol1501@gmail.com>
2025-03-15 22:23:35 +0900

    자바 성적처리 프로그램

--- Java/GradeManager/src/GradeOutput.java
+++ Java/GradeManager/src/GradeOutput.java
@@ -1,5 +1,12 @@
 
 public class GradeOutput {
+	int e;
+	int m;
+	
+	void input_grade(int a, int b) {
+		e = a;
+		m = b;
+	} 
 
 	public static void main(String[] args) {
 		Grade g1, g2;
@@ -12,3 +19,4 @@ public class GradeOutput {
 		g2.output_grad();
 	}
 }
+
